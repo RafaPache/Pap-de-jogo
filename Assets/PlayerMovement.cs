35d82e2103463fbcb20d36deca8a3fefb470d7b2
RafaPache <rafaelpachecooficiall@gmail.com>
2024-05-21 12:02:48 +0000

    Jogador pula

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jogadormovimento : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 9, 0);
+        }
     }
 }
