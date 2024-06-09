68f8b09cd264929235492c8ffdca6908c4cdc62a
RafaPache <rafaelpachecooficiall@gmail.com>
2024-06-09 11:38:57 +0000

    NOVO UPDATE
    BACKUP

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,23 +6,50 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
+    [SerializeField]private float moveSpeed = 7f;
+    [SerializeField]private float jumpForce = 14f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 14f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("correr", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("correr", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("correr", false);
         }
     }
 }
